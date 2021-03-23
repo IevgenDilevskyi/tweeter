1775a3d0cb290b3ebd0b48ba02e0cd3d8dd2ddb1
IevgenDilevskyi <e.dilevskiy@gmail.com>
2021-03-23 23:10:33 +0000

    Implement char counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,17 @@
+// const { length } = require("body-parser");
+
+$(document).ready(function() {
+  $('#tweet-text').on('input', function(){
+    let textLength = $('#tweet-text').val().length; // Count the length of text entered
+    let count = 140 - (textLength);
+    console.log(count);
+    const counter = $(this).siblings(".bottomLine").find('.counter') // Traversing DOM up and down
+    counter.text(count)
+    
+    if (count <= 0) { // If users exceed the 140 character limit, add class with corresponding CSS properties
+      counter.addClass('char-over')
+    } else {
+        counter.removeClass('char-over')
+      }
+  })
+});
