bc93810156a78edd1c2b124826a131da817ff339
IevgenDilevskyi <e.dilevskiy@gmail.com>
2021-03-24 21:24:11 +0000

    Add Ajax request for form submission

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -9,10 +9,10 @@
 $(document).ready(function() {
 
   const createTweetElement = function(tweetData) {
-    const tweet = $(`
+    const $tweet = $(`
     <article class="tweet">
       <header>
-        <h4> ${tweetData.user.avatars} ${tweetData.user.name} </h4>
+        <h4> <img src="${tweetData.user.avatars}"> ${tweetData.user.name} </h4>
         <h5> ${tweetData.user.handle} </h5>
       </header>
       <p>${tweetData.content.text}</p>
@@ -21,9 +21,9 @@ $(document).ready(function() {
         <div class="likes"> <i class="fab fa-font-awesome-flag"></i> <i class="fas fa-retweet"></i><i class="fas fa-heart"></i></div>
       </footer>
     </article>`);
-    return tweet
+    return $tweet
   }
-
+/*-------TEST for createTweetElement function--------
   const tweetData = {
     "user": {
       "name": "Newton",
@@ -40,20 +40,61 @@ $(document).ready(function() {
   console.log($createdTweet);
   
   $('#tweets').append($createdTweet);
+*/
+
+  const renderTweets = function(tweets) {
+    for (let tweet of tweets) {   // loops through tweets
+      let $result = createTweetElement(tweet) // calls createTweetElement for each tweet
+      $('#tweets').append($result);  // takes return value and appends it to the tweets container
+    }
+  }
+
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
+
+  renderTweets(data);
+
+  $(".new-tweet form").on("submit", function (event) {
+    event.preventDefault(); // Prevent default action
+    const $serialized = $( this ).serialize(); //Create jQuery string out of form data (text=....)
+    console.log('this', this);
+    console.log($serialized);
+
+    $.ajax({ url: "/tweets", data: $serialized, type: 'POST' }) // Ajax request sending serialized data to /tweets URL
+    .then(function (result) {
+      console.log('Success: ', result);
+    });
+
+  })
+
+
 
 });
 
-// const markup = `
-//  <div class="person">
-//     <h2>
-//         ${person.name}
-//     </h2>
-//     <p class="location">${person.city}</p>
-//     <p class="bio">${person.bio}</p>
-//  </div>
-// `;
 
-{/* <article>
+/*{ <article>
             <header>
               <h4> <i class="far fa-meh-blank"></i>Newton </h4>
               <h5>@SirIsaac</h5>
@@ -65,4 +106,4 @@ $(document).ready(function() {
               <div class="date">10 days ago</div>
               <div class="likes"> <i class="fab fa-font-awesome-flag"></i> <i class="fas fa-retweet"></i><i class="fas fa-heart"></i></div>
             </footer>
-</article> */}
+</article> }*/
